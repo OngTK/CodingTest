c49e4886bea77592193ba14ef4fe49cce44ccea9
OngTK <dhdxorud@gmail.com>
2026-01-09 12:13:55 +0900

    백준 7287

---
+++ src/baekjoon/Q7287.java
@@ -0,0 +1,17 @@
+package baekjoon;
+/**
+ * 문제
+ *      자신이 백준 온라인 저지(BOJ)에서
+ *      맞은 문제의 수와 아이디를 그대로 출력하는 프로그램을 작성
+ * 입력
+ *      X
+ * 출력
+ *      첫 줄에 맞은 문제의 수, 둘재 줄에 아이디를 출력
+ * */
+
+public class Q7287 {
+    public static void main(String[] args) {
+        System.out.println(163);
+        System.out.println("moonong");
+    } // main end
+} // class end
