9bbac829381899e7d8e125ae6c2f4cf86f2d751a
OngTK <dhdxorud@gmail.com>
2026-01-23 13:08:47 +0900

    백준 11727

---
+++ src/baekjoon/Q11727.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 백준 11727
+ * 2×n 타일링 2
+ *
+ * 문제
+ *     2×n 직사각형을 1×2, 2×1과 2×2 타일로 채우는 방법의 수를 구하는 프로그램을 작성
+ * 입력
+ *     첫째 줄에 n
+ * 출력
+ *     첫째 줄에 2×n 크기의 직사각형을 채우는 방법의 수를 10,007로 나눈 나머지를 출력
+ *
+ * 규칙
+ * 1 → 3 → 5 → 11 → 21 → ...
+ * */
+
+public class Q11727 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[1001];
+        arr[0] = arr[1] = 1;
+
+        for(int i = 2 ; i<=n ; i++){
+            arr[i] = (arr[i-2]*2 + arr[i-1]) % 10007;
+        }
+        System.out.println(arr[n]);
+    } // main end
+} // class end
